f7ef4cadfde2234c0085cf9a586a462f8cfb5eb5
likenomnom <wangtreran@outlook.com>
2023-03-05 12:43:19 +0800

    refactor:重构 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { App} from './App'
+import {createRouter} from 'vue-router'
+import { routes } from './components/config/routes'
+import { history } from './shared/history'
 
-createApp(App).mount('#app')
+const router = createRouter({ history, routes})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+ 
