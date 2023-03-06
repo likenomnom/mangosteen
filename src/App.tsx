e8dc79d378890709d7c6698912f7b5209b88c004
likenomnom <wangtreran@outlook.com>
2023-03-06 17:56:48 +0800

    feature: CSS初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
   setup(){
-    return () => <>
+    return () => (
     <div>
       <RouterView/>
     </div> 
-    
-    </>
+    )
   }
 })
