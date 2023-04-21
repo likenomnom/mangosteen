0b840ffada9dcf19335cf20b9fd5b814d21e2f49
likenomnom <wangtreran@outlook.com>
2023-04-21 16:44:15 +0800

    创建TagEdit

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -54,7 +54,7 @@ export const routes: RouteRecordRaw[] = [
     path: '/tags', component: TagPage,
     children: [
       {path: 'create', component: TagCreate},
-      {path: ':id', component: TagEdit}
+      {path: ':id/edit', component: TagEdit}
     ]
   }
 ]
