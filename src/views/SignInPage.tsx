24784952b3151b8ccc910bb97185e3e735091a3b
likenomnom <wangtreran@outlook.com>
2023-05-08 16:58:44 +0800

    登录成功后保存 jwt 至 localStorage

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -8,6 +8,7 @@ import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 import { http } from '../shared/HTTP';
 import { useBool } from '../hooks/useBool';
+import { history } from '../shared/history';
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -21,7 +22,6 @@ export const SignInPage = defineComponent({
     const refValidationCode = ref<any>()
     const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
     const onSubmit = async (e: Event) => {
-      console.log('submit')
       e.preventDefault()
       Object.assign(errors, {
         email: [], code: []
@@ -31,10 +31,12 @@ export const SignInPage = defineComponent({
         { key: 'email', type: 'pattern', regex: /.+@.+/, message: '必须是邮箱地址' },
         { key: 'code', type: 'required', message: '必填' },
       ]))
-      if(!hasError(errors)){
-        const response = await http.post('/session', formData)
+      if (!hasError(errors)) {
+        const response = await http.post<{ jwt: string }>('/session', formData)
+        localStorage.setItem('jwt', response.data.jwt)
+        history.push('/')
       }
-      }
+    }
       const onError = (error: any) => {
         if (error.response.status === 422) {
           Object.assign(errors, error.response.data.errors)
