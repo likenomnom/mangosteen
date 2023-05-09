2de111c81d495daf63ff66cd8ea8186aeb842b41
likenomnom <wangtreran@outlook.com>
2023-05-09 11:35:55 +0800

    每个用户只看一遍广告

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -9,6 +9,7 @@ import s from './SignInPage.module.scss';
 import { http } from '../shared/HTTP';
 import { useBool } from '../hooks/useBool';
 import { history } from '../shared/history';
+import { useRoute, useRouter } from 'vue-router';
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
@@ -21,6 +22,8 @@ export const SignInPage = defineComponent({
     })
     const refValidationCode = ref<any>()
     const { ref: refDisabled, toggle, on: disabled, off: enable } = useBool(false)
+    const router = useRouter()
+    const route = useRoute()
     const onSubmit = async (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -34,7 +37,9 @@ export const SignInPage = defineComponent({
       if (!hasError(errors)) {
         const response = await http.post<{ jwt: string }>('/session', formData)
         localStorage.setItem('jwt', response.data.jwt)
-        history.push('/')
+        // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
+        const returnTo = route.query.return_to?.toString()
+        router.push(returnTo || '/')
       }
     }
       const onError = (error: any) => {
@@ -64,7 +69,6 @@ export const SignInPage = defineComponent({
                 <Icon class={s.icon} name="mangosteen" />
                 <h1 class={s.appName}>山竹记账</h1>
               </div>
-              <div>{JSON.stringify(formData)}</div>
               <Form onSubmit={onSubmit}>
                 <FormItem label="邮箱地址" type="text"
                   placeholder='请输入邮箱，然后点击发送验证码'
