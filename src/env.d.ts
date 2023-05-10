028900fb755b60ed3d185cf2b57c0bf3ae268d35
likenomnom <wangtreran@outlook.com>
2023-05-10 11:33:48 +0800

    创建mock模块

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,6 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+
+type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
