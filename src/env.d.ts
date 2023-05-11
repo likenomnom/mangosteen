e0bfafe454192644ef3fb2900ddeb6cb8383976b
likenomnom <wangtreran@outlook.com>
2023-05-11 16:18:24 +0800

    封装Tag组件

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,12 +7,21 @@ declare module '*.vue' {
   export default component
 }
 
-
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+
 type Tag = {
   id: number,
   user_id: number,
   name: string,
   sign: string,
   kind: expenses | income
+}
+
+type Resources<T = any> = {
+  resources: T[]
+  pager: {
+    page: number,
+    per_page: number,
+    count: number
+  }
 }
