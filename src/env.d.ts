288d92f5da9c73df5b34d58308b52754ab6f9303
likenomnom <wangtreran@outlook.com>
2023-05-12 10:28:22 +0800

    格式化

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-// <reference types=vite/client />
+// <reference types="vite/client" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
@@ -6,43 +6,43 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-declare module '*.scss' {
-  const content: Record<string, any> = {}
-  export default content
-}
-
-
-type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+type JSONValue =
+  | null
+  | boolean
+  | string
+  | number
+  | JSONValue[]
+  | Record<string, JSONValue>;
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
-  kind: expenses | income
-}
+  id: number;
+  user_id: number;
+  name: string;
+  sign: string;
+  kind: expenses | income;
+};
 type Item = {
-  id: number
-  user_id: number
-  amount: number
-  tags_id: number[]
-  happen_at: string
-  kind: expenses | income
-}
+  id: number;
+  user_id: number;
+  amount: number;
+  tags_id: number[];
+  happen_at: string;
+  kind: expenses | income;
+};
 
 type Resources<T = any> = {
   resources: T[]
   pager: {
-    page: number,
-    per_page: number,
-    count: number
-  }
-}
+    page: number;
+    per_page: numbe;
+    count: number;
+  };
+};
 
 type Resource<T> = {
-  resource: T
-}
+  resource: T;
+};
 
 type ResourceError = {
-  errors: Record<string, string[]>
-}
+  errors: Record<string, string[]>;
+};
