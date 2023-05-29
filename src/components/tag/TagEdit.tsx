18f35c45e0503392d5b52e25e2778cbb71ba8337
likenomnom <wangtreran@outlook.com>
2023-05-29 11:03:22 +0800

    格式化

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,18 +1,18 @@
-import { defineComponent } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { MainLayout } from '../../layouts/MainLayout';
-import { BackIcon } from '../../shared/BackIcon';
-import { Button } from '../../shared/Button';
-import s from './Tag.module.scss';
-import { TagForm } from './TagForm';
-import { Dialog } from 'vant';
-import { http } from '../../shared/Http';
+import { Dialog } from 'vant'
+import { defineComponent } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { MainLayout } from '../../layouts/MainLayout'
+import { BackIcon } from '../../shared/BackIcon'
+import { Button } from '../../shared/Button'
+import { http } from '../../shared/Http'
+import s from './Tag.module.scss'
+import { TagForm } from './TagForm'
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute()
     const numberId = parseInt(route.params.id!.toString())
-    if(Number.isNaN(numberId)){
-      return ()=> <div>id 不存在</div>
+    if (Number.isNaN(numberId)) {
+      return () => <div>id 不存在</div>
     }
     const router = useRouter()
     const onError = ()=>{
@@ -23,25 +23,41 @@ export const TagEdit = defineComponent({
         title:'确认',
         message:'你真的要删除吗？'
       })
-      await http.delete(`/tags/${numberId}`, {
-        withItems: options?.withItems ? 'true' : 'false'
-      }).catch(onError)
+      await http
+        .delete(`/tags/${numberId}`, {
+          withItems: options?.withItems ? 'true' : 'false',
+        })
+        .catch(onError)
       router.back()
     }
     return () => (
-      <MainLayout>{{
-        title: () => '编辑标签',
-        icon: () => <BackIcon />,
-        default: () => <>
-          <TagForm id={numberId} />
-          <div class={s.actions}>
-          <Button level='danger' class={s.removeTags}
-              onClick={()=>onDelete()}>删除标签</Button>
-            <Button level='danger' class={s.removeTagsAndItems}
-              onClick={()=>onDelete({withItems: true})}>删除标签和记账</Button>
-          </div>
-        </>
-      }}</MainLayout>
+      <MainLayout>
+        {{
+          title: () => '编辑标签',
+          icon: () => <BackIcon />,
+          default: () => (
+            <>
+              <TagForm id={numberId} />
+              <div class={s.actions}>
+                <Button
+                  level="danger"
+                  class={s.removeTags}
+                  onClick={() => onDelete()}
+                >
+                  删除标签
+                </Button>
+                <Button
+                  level="danger"
+                  class={s.removeTagsAndItems}
+                  onClick={() => onDelete({ withItems: true })}
+                >
+                  删除标签和记账
+                </Button>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
     )
-  }
+  },
 })
