edb56908ca213bd8327902664d7928e1b6fee5ee
likenomnom <wangtreran@outlook.com>
2023-05-29 11:07:26 +0800

    mock tagEdit

--- src/shared/HTTP.tsx
+++ src/shared/HTTP.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { mockItemCreate, mockSession, mockTagIndex, mockTagShow } from "../mock/mock";
+import { mockItemCreate, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
 
 type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
 type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
@@ -46,6 +46,9 @@ const mock = (response: AxiosResponse) => {
       case 'itemCreate':
         [response.status, response.data] = mockItemCreate(response.config)
         return true
+        case 'tagEdit':
+      [response.status, response.data] = mockTagEdit(response.config)
+      return true
     }
   return false
 }
