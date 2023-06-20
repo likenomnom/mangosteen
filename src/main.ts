35ef30dbc592af3c09a283491374e78aa873c6cc
likenomnom <wangtreran@outlook.com>
2023-06-20 16:02:37 +0800

    解决登录页面后退按钮逻辑错误的bug

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { useMeStore } from './stores/useMestore';
+import { useMeStore } from './stores/useMeStore';
 import { routes } from './config/routes';
 import { createApp } from 'vue'
 import { App } from './App'
@@ -6,27 +6,27 @@ import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
 import { createPinia, storeToRefs } from 'pinia';
+import { Dialog } from 'vant';
+
 
 const router = createRouter({ history, routes })
-const pinia = createPinia ()
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
 
 const meStore = useMeStore()
-const {mePromise} = storeToRefs(meStore)
+const { mePromise } = storeToRefs(meStore)
 meStore.fetchMe()
-
 
 
 const whiteList: Record<string, 'exact' | 'startsWith'> = {
   '/': 'exact',
-  '/start': 'exact',
+  '/items': 'exact',
   '/welcome': 'startsWith',
   '/sign_in': 'startsWith',
 }
-
 router.beforeEach((to, from) => {
   for (const key in whiteList) {
     const value = whiteList[key]
@@ -39,10 +39,6 @@ router.beforeEach((to, from) => {
   }
   return mePromise!.value!.then(
     () => true,
-    () => '/sign_in?return_to=' + to.path
+    () => '/sign_in?return_to=' + from.path
   )
 })
-function createPinia() {
-  throw new Error('Function not implemented.');
-}
-
