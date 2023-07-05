ab8e9b92071ae39ac075328ce54dbde344146d99
likenomnom <wangtreran@outlook.com>
2023-07-05 17:27:43 +0800

    删除没有必要的 dev 环境判断

--- src/shared/HTTP.tsx
+++ src/shared/HTTP.tsx
@@ -29,15 +29,7 @@ export class Http {
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
   }
 }
-function isDev() {
-  if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1' && location.hostname !== '192.168.3.57') {
-    return false
-  }
-
-  return true
-}
-
-export const http = new Http(isDev() ? 'api/v1' : 'http://121.196.236.94:3000/api/v1')
+export const http = new Http(DEBUG ? 'api/v1' : 'http://121.196.236.94:3000/api/v1')
 
 http.instance.interceptors.request.use((config) => {
   const jwt = localStorage.getItem('jwt')
@@ -93,14 +85,6 @@ if (DEBUG) {
       mockTagShow
     }) => {
       const mock = (response: AxiosResponse) => {
-        if (
-          true ||
-          (location.hostname !== 'localhost' &&
-            location.hostname !== '127.0.0.1' &&
-            location.hostname !== '192.168.3.57')
-        ) {
-          return false
-        }
         switch (response.config?._mock) {
           case 'tagIndex':
             ;[response.status, response.data] = mockTagIndex(response.config)
